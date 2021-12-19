35b683b86de175568ebf6d5bc2a0924eeb3462b6
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2021-12-19 11:08:34 -0700

    Update Data_Analysis.R

--- Data_Analysis.R
+++ Data_Analysis.R
@@ -1,1 +1,10 @@
 # Load Packages
+library(readr)
+library(dplyr)
+library(magrittr)
+library(tximport)
+Sample_Table=read_csv("SraRunTable.txt") %>%
+  select('Sample Name',source_name,treatment,Cell_Line,Cell_type,time_point) %>%
+  slice(seq(1,48,by=4))
+tximport(files = ,type = "salmon",tx2gene = ,
+           ignoreTxVersion = TRUE)
